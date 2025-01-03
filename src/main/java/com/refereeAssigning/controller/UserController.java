c22c15b614f3e32335869d92a2436158efd26bfa
Joey Fisher <70974815+StratFrea@users.noreply.github.com>
2025-01-03 10:53:40 -0800

    Started UserController (#15)

--- src/main/java/com/refereeAssigning/controller/UserController.java
+++ src/main/java/com/refereeAssigning/controller/UserController.java
@@ -1,4 +1,65 @@
 package com.refereeAssigning.controller;
 
+import com.refereeAssigning.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.refereeAssigning.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Create a new user
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    // Get a list of all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    // Get a specific user by their ID
+    @GetMapping("/{id")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    // Update a user's details
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        User updateduser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updateduser);
+    }
+
+    // Delete a user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Check if email exists
+    @GetMapping("/exists")
+    public ResponseEntity<Boolean> checkEmailExists(@RequestParam String email) {
+        boolean exists = userService.emailExists(email);
+        return ResponseEntity.ok(exists);
+    }
+
+    //TODO: Create more operations when needed
 }
