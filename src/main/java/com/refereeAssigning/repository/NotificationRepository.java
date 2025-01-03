df3ef1eef07f82aa0fca3cc17013d92a91e69189
Joey Fisher <70974815+StratFrea@users.noreply.github.com>
2025-01-02 17:07:17 -0800

    Created NotificationRepository (#7)

--- src/main/java/com/refereeAssigning/repository/NotificationRepository.java
+++ src/main/java/com/refereeAssigning/repository/NotificationRepository.java
@@ -12,11 +12,9 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     // Find notifications by user ID
     List<Notification> findByUserId(Long userId);
 
-    // Find unread notifications for a user
-    List<Notification> findByUserIdAndIsReadFalse(Long userId);
+    // TODO Find unread notifications for a user
 
-    // Find notifications by type (e.g., email, SMS, push)
-    List<Notification> findByType(String type);
+
 
     // Delete all notifications for a specific user
     void deleteByUserId(Long userId);
