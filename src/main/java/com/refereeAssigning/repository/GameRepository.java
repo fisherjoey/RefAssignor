62feffa2bd182541a19791334d86280d033c7499
Joey Fisher <70974815+StratFrea@users.noreply.github.com>
2025-01-02 17:08:24 -0800

    Created custom query to search for users in games (#6)

--- src/main/java/com/refereeAssigning/repository/GameRepository.java
+++ src/main/java/com/refereeAssigning/repository/GameRepository.java
@@ -2,6 +2,7 @@ package com.refereeAssigning.repository;
 
 import com.refereeAssigning.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,18 +13,16 @@ import java.util.List;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+
     // Find games by home team
     List<Game> findByHomeTeam(String homeTeam);
 
     // Find games by away team
     List<Game> findByAwayTeam(String awayTeam);
 
-    // Find games where a specific referee is assigned
+    // Find games by Assigned Referee
+    @Query("SELECT g FROM Game g WHERE :refereeId MEMBER OF g.assignedReferees")
     List<Game> findByAssignedRefereeId(Long refereeId);
 
-    // Find games by status (e.g., completed, scheduled, etc.)
-    List<Game> findByStatus(String status);
-
-    // Custom query to find games by date range
-    List<Game> findByGameDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+    List<Game> findByDateTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
